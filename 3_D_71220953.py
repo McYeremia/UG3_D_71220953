ff4198ee5c32c079eef2442c2eff9561a1c71257
McYeremia <117885019+McYeremia@users.noreply.github.com>
2023-02-22 18:59:54 +0700

    Add files via upload

---
+++ 3_D_71220953.py
@@ -0,0 +1,16 @@
+a1 = int(input('Masukkan awal deret: '))
+a2 = int(input('Masukkan akhir deret: '))
+j = 0
+
+while j < a2:
+    if a1 % 6 != 0 and a1 % 8 != 0:
+        print(a1, end=' ')
+        j += 1
+    a1 += 1
+    if a1 == a2:
+        break
+
+#ternary
+#while j < a2:
+    #j += 1 and print(a1, end=' ') if a1 % 6 != 0 and a1 % 8 != 0 else print() and (a1 += 1) if a1 == a2 else print()
+    #break
